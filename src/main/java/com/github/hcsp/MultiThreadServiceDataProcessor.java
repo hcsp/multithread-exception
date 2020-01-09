6327a262a3bbc6d4c04c57045b326180a2e9104e
JayChenFE <jaychenfe@foxmail.com>
2020-01-09 11:29:51 +0800

    Jay homework (#22)
    Jay homework (#22)

--- src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
+++ src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
@@ -1,8 +1,10 @@
 package com.github.hcsp;
 
 import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MultiThreadServiceDataProcessor {
     // 线程数量
@@ -23,11 +25,17 @@ public class MultiThreadServiceDataProcessor {
                         ? allData.size() / threadNumber
                         : allData.size() / threadNumber + 1;
         List<List<Object>> dataGroups = Lists.partition(allData, groupSize);
-
+        AtomicBoolean hasException = new AtomicBoolean(false);
         try {
             List<Thread> threads = new ArrayList<>();
             for (List<Object> dataGroup : dataGroups) {
                 Thread thread = new Thread(() -> dataGroup.forEach(remoteService::processData));
+                // 使用 setUncaughtExceptionHandler 线程处理异常
+                thread.setUncaughtExceptionHandler((t, e) -> {
+                    hasException.set(true);
+                    // todo:在实际业务中通过设定线程名,序列化对象实体等方法,写入日志,并补偿发送dataGroup
+                    System.out.println("线程出错,数据为" + dataGroup);
+                });
                 thread.start();
                 threads.add(thread);
             }
@@ -35,7 +43,7 @@ public class MultiThreadServiceDataProcessor {
             for (Thread thread : threads) {
                 thread.join();
             }
-            return true;
+            return !hasException.get();
         } catch (Exception e) {
             return false;
         }
