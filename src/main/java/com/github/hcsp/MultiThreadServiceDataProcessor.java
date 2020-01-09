344f76dbce4939fc2c92a7bc2b89236df8d3b6ec
JayChenFE <jaychenfe@foxmail.com>
2020-01-09 11:28:14 +0800

    用局部变量存储状态

--- src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
+++ src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
@@ -4,14 +4,13 @@ import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MultiThreadServiceDataProcessor {
     // 线程数量
     private final int threadNumber;
     // 处理数据的远程服务
     private final RemoteService remoteService;
-
-    private boolean hasException = false;
 
     public MultiThreadServiceDataProcessor(int threadNumber, RemoteService remoteService) {
         this.threadNumber = threadNumber;
@@ -26,14 +25,14 @@ public class MultiThreadServiceDataProcessor {
                         ? allData.size() / threadNumber
                         : allData.size() / threadNumber + 1;
         List<List<Object>> dataGroups = Lists.partition(allData, groupSize);
-
+        AtomicBoolean hasException = new AtomicBoolean(false);
         try {
             List<Thread> threads = new ArrayList<>();
             for (List<Object> dataGroup : dataGroups) {
                 Thread thread = new Thread(() -> dataGroup.forEach(remoteService::processData));
                 // 使用 setUncaughtExceptionHandler 线程处理异常
                 thread.setUncaughtExceptionHandler((t, e) -> {
-                    hasException = true;
+                    hasException.set(true);
                     // todo:在实际业务中通过设定线程名,序列化对象实体等方法,写入日志,并补偿发送dataGroup
                     System.out.println("线程出错,数据为" + dataGroup);
                 });
@@ -44,9 +43,9 @@ public class MultiThreadServiceDataProcessor {
             for (Thread thread : threads) {
                 thread.join();
             }
-            return !hasException;
+            return !hasException.get();
         } catch (Exception e) {
-            return !hasException;
+            return false;
         }
     }
 }
