7205d84a91a7be862bc8b2207c9ef98fc50b02e3
1104049235 <1104049235@qq.com>
2020-05-25 17:52:52 +0800

    修复异常处理 (#40)
    修复异常处理 (#40)

--- src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
+++ src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
@@ -4,6 +4,8 @@ import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.concurrent.*;
+
 public class MultiThreadServiceDataProcessor {
     // 线程数量
     private final int threadNumber;
@@ -17,27 +19,31 @@ public class MultiThreadServiceDataProcessor {
 
     // 将所有数据发送至远程服务处理。若所有数据都处理成功（没有任何异常抛出），返回true；
     // 否则只要有任何异常产生，返回false
-    public boolean processAllData(List<Object> allData) {
+    public boolean processAllData(List<Object> allData)  {
         int groupSize =
                 allData.size() % threadNumber == 0
                         ? allData.size() / threadNumber
                         : allData.size() / threadNumber + 1;
         List<List<Object>> dataGroups = Lists.partition(allData, groupSize);
-
-        try {
-            List<Thread> threads = new ArrayList<>();
-            for (List<Object> dataGroup : dataGroups) {
-                Thread thread = new Thread(() -> dataGroup.forEach(remoteService::processData));
-                thread.start();
-                threads.add(thread);
-            }
-
-            for (Thread thread : threads) {
-                thread.join();
-            }
-            return true;
-        } catch (Exception e) {
-            return false;
+        ArrayList<Future<Boolean>> futures = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(dataGroups.size());
+        for (List<Object> dataGroup : dataGroups) {
+            Future submit = executorService.submit(new Callable() {
+                @Override
+                public Boolean call() throws Exception {
+                    dataGroup.forEach(remoteService::processData);
+                    return true;
+                }
+            });
+            futures.add(submit);
         }
+        for (Future<Boolean> future : futures) {
+            try {
+                Boolean aBoolean = future.get();
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
     }
 }
