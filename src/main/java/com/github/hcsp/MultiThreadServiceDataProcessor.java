c02931682d643be9b6fd1f0279d831995f276f81
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2022-07-18 16:01:10 +0800

    使用 Callable 代替 Runnable (#52)
    使用 Callable 代替 Runnable (#52)

--- src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
+++ src/main/java/com/github/hcsp/MultiThreadServiceDataProcessor.java
@@ -1,18 +1,24 @@
 package com.github.hcsp;
 
 import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MultiThreadServiceDataProcessor {
     // 线程数量
     private final int threadNumber;
     // 处理数据的远程服务
     private final RemoteService remoteService;
+    private final ExecutorService threadPool;
 
     public MultiThreadServiceDataProcessor(int threadNumber, RemoteService remoteService) {
         this.threadNumber = threadNumber;
         this.remoteService = remoteService;
+        this.threadPool = Executors.newFixedThreadPool(threadNumber);
     }
 
     // 将所有数据发送至远程服务处理。若所有数据都处理成功（没有任何异常抛出），返回true；
@@ -25,15 +31,17 @@ public class MultiThreadServiceDataProcessor {
         List<List<Object>> dataGroups = Lists.partition(allData, groupSize);
 
         try {
-            List<Thread> threads = new ArrayList<>();
+            List<Future<Object>> futures = new ArrayList<>();
             for (List<Object> dataGroup : dataGroups) {
-                Thread thread = new Thread(() -> dataGroup.forEach(remoteService::processData));
-                thread.start();
-                threads.add(thread);
+                Future<Object> submit = threadPool.submit(() -> {
+                    dataGroup.forEach(remoteService::processData);
+                    return null;
+                });
+                futures.add(submit);
             }
 
-            for (Thread thread : threads) {
-                thread.join();
+            for (Future<Object> future : futures) {
+                future.get();
             }
             return true;
         } catch (Exception e) {
